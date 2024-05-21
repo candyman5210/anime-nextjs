5de1a2482a44be1aa6e9891d2ff5eb61caa52571
candyman5210 <odhiambo.newton@gmx.com>
2024-05-21 16:16:01 +0300

    interface added

---
+++ src/app/dashboard/about/page.tsx
@@ -0,0 +1,11 @@
+const about = () => {
+    return (
+        <>
+        <h1>About</h1>
+        <p>anime details weill be displayed hapa</p>
+        <p>gvfhbrignuitrnuihniutunhmbiynuib</p>
+        <p>gbiurhnjnuibinbihnmjnjg</p></>
+      );
+}
+ 
+export default about;
